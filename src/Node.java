be0cecbb19165f6dd96268c760f48c35d3ee2c10
Elliot <Elliot.palmstadius@gmail.com>
2021-10-11 12:29:14 +0200

    Hej

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,6 @@
 public class Node {
+    mainFrame frame;
     public Node() {
-
+       frame = new mainFrame();
     }
 }
--- src/mainFrame.java
+++ src/mainFrame.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
 
 public class mainFrame {
     public mainFrame() {
@@ -14,13 +18,55 @@ public class mainFrame {
         private nodePanel nodePanel;
         private controlPanel controlPanel;
         public mainPanel(){
-
+            nodePanel = new nodePanel(600,600);
+            controlPanel = new controlPanel(200,600);
+            add(nodePanel);
+            add(controlPanel);
         }
 
-        private class nodePanel {
+        private class nodePanel extends JPanel{
+            public nodePanel(int w, int h){
+                setPreferredSize(new Dimension(w,h));
+            }
         }
 
-        private class controlPanel {
+        private class controlPanel extends JPanel implements ActionListener {
+            private LinkedList<JButton> buttons;
+            public controlPanel(int w, int h){
+                setPreferredSize(new Dimension(w,h));
+                buttons = new LinkedList<>();
+                JButton buttonConnect = new JButton("Connect a node");
+                buttons.add(buttonConnect);
+
+                JButton buttonDisconnectChild = new JButton("Disconnect a child node");
+                buttons.add(buttonDisconnectChild);
+
+                JButton buttonDisconnectParent = new JButton("Disconnect a parent node");
+                buttons.add(buttonDisconnectParent);
+
+                JButton buttonDisconnectHead = new JButton("Disconnect a head node");
+                buttons.add(buttonDisconnectHead);
+
+                JButton buttonStart = new JButton("Start game");
+                buttons.add(buttonStart);
+
+                JButton buttonExit = new JButton("Exit game");
+                buttons.add(buttonExit);
+
+                JButton buttonReset = new JButton("Reset");
+                buttons.add(buttonReset);
+
+                for (JButton b : buttons) {
+                    b.setPreferredSize(new Dimension(getPreferredSize().width - 20, 50));
+                    b.setFont(new Font("Arial", Font.PLAIN, 10));
+                    b.addActionListener(this);
+                }
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
         }
     }
 }
